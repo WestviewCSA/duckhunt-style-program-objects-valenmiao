5b5ae3652f6c611f38a83b1774093c389307cb3b
1929056 <1929056@01-18107.pusd.dom>
2025-10-24 10:01:11 -0700

    f

--- Duck Hunt Style Game 2025/src/Duck.java
+++ Duck Hunt Style Game 2025/src/Duck.java
@@ -1,11 +1,14 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.net.URL;
-
 // The Duck class represents a picture of a duck that can be drawn on the screen.
+
+
 public class Duck {
     // Instance variables (data that belongs to each Duck object)
     private Image img;               // Stores the picture of the duck
@@ -16,16 +19,19 @@ public class Duck {
     private double scaleY;           
 
     // Variables to control the location (x and y position) of the duck
-    private double x;                
-    private double y;        
+    private int x;                
+    private int y;        
     
     //variables for speed
     private int vx;
     private int vy;
-
+    //shot
+ 
+    //debugging variable
+    public boolean debugging = true;
     // Constructor: runs when you make a new Duck object
     public Duck() {
-        img = getImage("/imgs/duck.gif"); // Load the image file
+        img = getImage("/imgs/PIskel candy.gif"); // Load the image file
         
         tx = AffineTransform.getTranslateInstance(0, 0); // Start with image at (0,0)
         
@@ -34,6 +40,8 @@ public class Duck {
         scaleY = 1.0;
         x = 0;
         y = 0;
+        vx = 5;
+        vy = 5;
 
         init(x, y); // Set up the starting location and size
     }
@@ -64,6 +72,7 @@ public class Duck {
     	this.vx = vx;
     	this.vy = vy;
     }
+
     
     
     // Changes the picture to a new image file
@@ -72,23 +81,113 @@ public class Duck {
         init(x, y); // keep same location when changing image
     }
     
-    //update any variables for the object such as x, y, vx, vy
-    public void update() {
+    //update any variables for the object such as x, y, vx, vy......
+    public void update() { 
+   	x = x + vx;
+    if (vx > 50) {
+    	
+    }
+    if (vx < -50) {
+    	
+    }
+    if (vy> 50) {
+    	
+    }
+    if (vy < -50) {
     	
     }
     
     
     
+   	if (y < 400) {
+   	y = y + vy;
+   	
+   	}
+  
+    	if(x>=450) {
+    		vx = vx * -1;
+    		if (vx>0) {
+    			vx = vx +1;
+    			System.out.println(vx);
+    		}
+    		else {
+    			vx = vx - 1;
+    			System.out.println(vx);
+    		}
+    	}
+    	if(x <= -130) {
+    		vx = vx * -1;
+    		if (vx>0) {
+    			vx = vx +1;
+    			System.out.println(vx);
+    			
+    		}
+    		else {
+    			vx = vx - 1;
+    			System.out.println(vx);
+    		}
+    	}
+    	
+    	if(y <=-130) {
+    	vy = vy * -1;
+    		if (vy>0) {
+    			vy = vy +1;
+    			System.out.println(vy);
+			
+    		}
+    		else {
+    			vy = vy -1;
+    			System.out.println(vy);
+    		}
+    	}
+    	
+    	if (vx == 0 && vy > 10) {
+    		if(y >= 200) {
+    			vy = - (int)(Math.random()*8+3);
+    		vx = (int)(Math.random()*8+3);
+    		if(Math.random()<0.5) {
+    			vx = vx * +1;
+    			}
+    		
+    		}
+    	}
+  
+    
+  
+    	
+    	
+    if(y >= 200 && vx!=0) {
+   	vy = vy * -1;
+   		if (vy>0) {
+   			vy = vy +1;
+   			System.out.println(vy);
+		
+   		}
+   		else {
+   			vy = vy - 1;
+   			System.out.println(vy);
+	}
+    }
+    }    
     // Draws the duck on the screen
     public void paint(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;   // Graphics2D lets us draw images
-        g2.drawImage(img, tx, null);      // Actually draw the duck image
+        Graphics2D g2 = (Graphics2D) g;
+        if (vx<0) {
+        	tx.scale(-1, 1);
+        	tx.translate(-img.getWidth(null), 0);
+        }
+        g2.drawImage(img, tx, null);
         update();
-        init(x,y);
+        init(x, y);
+        //create a green hitbox
+        if(debugging) {
+        	g.setColor(Color.magenta);
+        	g.drawRect((int) x + 150, (int) y + 140, 30, 30); 
+        }
     }
     
     // Setup method: places the duck at (a, b) and scales it
-    private void init(double a, double b) {
+    private void init(double a,double b){
         tx.setToTranslation(a, b);        // Move the image to position (a, b)
         tx.scale(scaleX, scaleY);         // Resize the image using the scale variables
     }
@@ -111,11 +210,79 @@ public class Duck {
         scaleY = sy;
         init(x, y);  // Keep current location
     }
+ 
 
     // NEW: Method to set location
-    public void setLocation(double newX, double newY) {
+    public void setLocation(int newX, int newY) {
         x = newX;
         y = newY;
         init(x, y);  // Keep current scale
     }
+    public void setVelocity(int newX, int newY) {
+        vx = newX;
+        vy = newY;
+        init(x, y);  // Keep current scale
+    }
+    //Collision and collision logic
+    public boolean checkCollision(int mX, int mY) {
+    	
+    	Rectangle mouse = new Rectangle(mX - 140, mY - 140, 15, 15);
+    	Rectangle thisObject = new Rectangle((int)x, (int)y, 30, 30);
+    
+    	if (mouse.intersects(thisObject)) {
+
+    		changePicture("PIskel candy (1).gif");
+    		return true;
+    		
+    	}
+    	else {
+    		return false;
+    	}
+   
+    	
+    
+    
+    
+    
+    
+    }
+    public double getX() {
+    	return this.x;
+    }
+    public double getY() {
+    	return this.y;
+    }
+    public double getVX() {
+    	return this.vx;
+    }
+    public double getVY() {
+    	return this.vy;
+    }
+    
+
+ 
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- Duck Hunt Style Game 2025/src/Frame.java
+++ Duck Hunt Style Game 2025/src/Frame.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -16,25 +17,80 @@ import javax.swing.Timer;
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
 	
 	//frame size
-	private int screenWidth = 900, screenHeight = 600;
-	private String title = "Duck Hunt";
+	private int screenWidth = 640, screenHeight = 640;
+	private String title = "feed the baby";
 	
 	
 	/**
 	 * Declare and instantiate (create) your objects here
 	 */
-	private Duck duckObject = new Duck();
+	int score = 0;
+
+	int x = 1;
+	int y = 400;
+	int frame = 0;
+	int fr = 0;
+	double scaleX = 1.0;
+	double scaleY = 1.0;
+	private Background myBackground = new Background();
+	private Duck duckObject = new Duck(1, 1, 1, 1);
+	private Kid kidObject = new Kid(x, y, 1, 1);
+	private MyCursor cursor = new MyCursor();
+	private Duck duckObject2 = new Duck(152, 1, 1, 1);
 	
 	public void paint(Graphics pen) {
+	if (frame == 0) {
+		duckObject.setVelocity((int)(Math.random()* 5) + score, (int)(Math.random()* 5) + score);
 		
+	}
 		//this line of code is to force redraw the entire frame
 		super.paintComponent(pen);
-		
+		//Background should be drawn before the objects as it's layer is last
+		myBackground.paint(pen);
 		//call paint for the object
 		//for objects, you call methods on them using the dot operator
 		//methods use always involve parenthesis
 		duckObject.paint(pen);
+		if (score > 5) {
+			duckObject2.paint(pen);
+			
+		}
+		kidObject.paint(pen);
+		cursor.paint(pen);
 		
+		frame = frame + 1;
+		if (fr == frame) {
+			cursor.changePicture("download.png");
+		}
+		if ((duckObject.getVX() ==0)&& kidObject.checkCollision((int)duckObject.getX(), (int)duckObject.getY())) {
+			cursor.ammoIncrement();
+			score = score+1;
+			System.out.println(score);
+			duckObject.changePicture("PIskel candy.gif");
+			duckObject.setLocation((int)(Math.random()* 100) , (int)(Math.random() * 100));
+			System.out.println(duckObject.getX() + ", " + duckObject.getY());
+			duckObject.setVelocity((int)(Math.random()* 5) + score, (int)(Math.random()* 5) + score);
+		
+		}
+		
+		if ((duckObject2.getVX() ==0)&& score > 5 && kidObject.checkCollision((int)duckObject2.getX(), (int)duckObject2.getY())) {
+			cursor.ammoIncrement();
+			score = score+1;
+			System.out.println(score);
+			duckObject2.changePicture("PIskel candy.gif");
+			duckObject2.setLocation((int)(Math.random()* 100) , (int)(Math.random() * 100));
+			System.out.println(duckObject2.getX() + ", " + duckObject2.getY());
+			duckObject2.setVelocity((int)(Math.random()* 5) + score, (int)(Math.random()* 5) + score);
+		
+		}
+		
+		if (cursor.ammoLeft() == -1) {
+			System.out.println("You have ran out of Ammo.");
+			System.out.println("Your score was " + score);
+			System.exit(0);
+		}
+		////////////////////////////////////around 120 velocityit breaks
+		///
 		
 		
 	}
@@ -44,6 +100,8 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	public void mouseClicked(MouseEvent mouse) {
 	    // Runs when the mouse is clicked (pressed and released quickly).
 	    // Example: You could use this to open a menu or select an object.
+	    
+	    
 	}
 
 	@Override
@@ -58,10 +116,35 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	    // Example: You could remove the highlight when the mouse moves away.
 	}
 
+
 	@Override
 	public void mousePressed(MouseEvent mouse) {
 	    // Runs when a mouse button is pressed down.
 	    // Example: You could start dragging an object here.
+		int Mx = mouse.getX() - 50;
+		int My = mouse.getY() - 50;
+		System.out.println(mouse.getX()+":"+mouse.getY());
+		//if (duckObject.checkCollision(Mx, My)) {
+			//duckObject.shot();
+		//}
+		System.out.println(duckObject.checkCollision(Mx, My));
+		if (duckObject.checkCollision(Mx, My) && cursor.ammoLeft() >=0) {
+			duckObject.setVelocity(0, 9);
+	
+		}
+		System.out.println(duckObject2.checkCollision(Mx, My));
+		if (duckObject2.checkCollision(Mx, My) && cursor.ammoLeft() >=0 && score>5) {
+			duckObject2.setVelocity(0, 9);
+	
+		}
+		if (cursor.ammoLeft() >=0) {
+		cursor.changePicture("download (1).png");
+	    fr = frame + 10;
+	    cursor.ammoDecrement();
+	    System.out.println("You have " + cursor.ammoLeft() + " ammo left");
+		}
+		System.out.println("score:" + score);
+
 	}
 
 	@Override
@@ -77,8 +160,22 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	 */
 	public void keyPressed(KeyEvent key) {
 		
-		System.out.println("from keyPressed method:"+key.getKeyCode());
+		//System.out.println("from keyPressed method:"+key.getKeyCode());
 		
+		if (65 == key.getKeyCode()) {
+			x = x -5;
+			kidObject.setLocation(x, y);
+			//System.out.println("a");
+		}
+		if (68 == key.getKeyCode()) {
+			x = x+5;
+			kidObject.setLocation(x, y);
+			//ddadadSystem.out.println("d");
+		}
+		//w87
+		//a65
+		//s83
+		//d68
 	}
 
 	/*
@@ -127,6 +224,13 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 		f.setLayout(new GridLayout(1,2));
 		f.addMouseListener(this);
 		f.addKeyListener(this);
+		
+		//Cursor icon Code
+		//Toolkit toolkit = Toolkit.getDefaultToolkit();
+		//Image image = toolkit.getImage("cursor.png");
+		//Cursor a = toolkit.createCustomCursor(image, new Point(this.getX(),this.getY()));
+		//this.setCursor (a);
+		
 		Timer t = new Timer(16, this);
 		t.start();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
